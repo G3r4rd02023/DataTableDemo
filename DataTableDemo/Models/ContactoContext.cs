99bb1dbd56c3fdde13444026a9c04c78974b1e34
Gerardo Lanza <glanza007@gmail.com>
2023-10-24 12:11:51 -0600

    Agregar archivos de proyecto.

---
+++ DataTableDemo/Models/ContactoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataTableDemo.Models
+{
+    public class ContactoContext : DbContext
+    {
+        public ContactoContext(DbContextOptions<ContactoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contacto> Contactos { get; set; }
+
+
+    }
+}
